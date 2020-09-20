319c443251819dd80f9fcda862ff8ceaec5fa436
Mathe Sharvadze <sharvadzemat@gmail.com>
2020-09-20 20:07:47 +0400

    django level one

---
+++ first_project/first_app/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from django.urls import path
+from first_app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    ]
